ee2e6f8bb31a574dceff8e84d9442946c6dd592f
unrealwork <unrealwork@gmail.com>
2018-09-21 22:32:18 +0300

    increase wait diagnostic timeout

--- client/src/test/diagnostic.test.ts
+++ client/src/test/diagnostic.test.ts
@@ -17,7 +17,7 @@ suite("Should diagnostics", () => {
         const docUri: Uri = getDocUri("simple.config");
         const document: TextDocument = await workspace.openTextDocument(docUri);
         await window.showTextDocument(document);
-        await sleep(1000);
+        await sleep(2000);
         const actualDiagnostic: Diagnostic[] = languages.getDiagnostics(docUri);
         strictEqual(actualDiagnostic.length, 1, "Incorrect number of messages for file");
         await commands.executeCommand("workbench.action.closeActiveEditor");
@@ -37,7 +37,7 @@ suite("Should diagnostics", () => {
         const docUri: Uri = getDocUri("simple.config");
         const document: TextDocument = await workspace.openTextDocument(docUri);
         await window.showTextDocument(document);
-        await sleep(1000);
+        await sleep(2000);
         const isEdited: boolean = await window.activeTextEditor.edit((editBuilder: TextEditorEdit) =>
             editBuilder.insert(new Position(0, 0), " "));
         ok(isEdited);
